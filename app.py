c919efac32a4cb2e164e220f3b2b3c9f121c68f1
Sagar Tiwari <sagartiwari.bmp@gmail.com>
2022-05-08 04:10:45 +0530

    First commit

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for, request, session, redirect
+from flask import Flask, render_template, flash, url_for, request, session, redirect
 from flask_pymongo import PyMongo
-import bcrypt
+from flask_mail import Mail, Message
+from flask_recaptcha import ReCaptcha
+from random import randint
 
 app = Flask(__name__)
 app.secret_key = 'mysecret'
@@ -8,7 +10,39 @@ app.secret_key = 'mysecret'
 app.config['MONGO_DBNAME'] = 'userlogin'
 app.config['MONGO_URI'] = 'mongodb+srv://sangam:Workinflow1@userlogin.jzluw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
+# Email Verification
+
+mail = Mail(app)
+
+app.config["MAIL_SERVER"] = 'smtp.gmail.com'
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USERNAME"] = 'sagartiwari.bmp@gmail.com'   # you have to give your email app password
+app.config['MAIL_PASSWORD'] = 'exmdmxjpfcabldyk'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+otp = randint(000000, 999999)
+
+
+# Recaptcha Verification
+
+recaptcha = ReCaptcha(app=app)
+
+app.config.update(dict(
+    RECAPTCHA_ENABLED=True,
+    RECAPTCHA_SITE_KEY="6LeKOaMZAAAAAI7L6TVsZa9A2t6-9LDVYSVqX9ZP",
+    RECAPTCHA_SECRET_KEY="6LeKOaMZAAAAAKw9nhAjnpzrzrC3R0YYRf-kKDH1",
+))
+
+recaptcha = ReCaptcha()
+recaptcha.init_app(app)
+
+
+#Routes
+
+
 mongo = PyMongo(app)
+
 
 @app.route('/')
 def index():
@@ -17,21 +51,40 @@ def index():
 
     return render_template('index.html')
 
+
+
 @app.route('/login', methods=['POST'])
 def login():
     users = mongo.db.users
-    login_user = users.find_one({'email' : request.form['email']})
+    login_user = users.find_one({'email': request.form['email']})
 
     if login_user:
         if request.form['password'] == login_user['password']:
             session['email'] = request.form['email']
-            return redirect(url_for('index'))
+            email = request.form['email']
+            msg = Message(subject='otp', sender='sagartiwari.bmp@gmail.com', recipients=[email])
+            msg.body = str(otp)
+            mail.send(msg)
+            return render_template('verify.html')
 
     return 'Invalid username/password combination'
 
+
+
+@app.route('/validate', methods=['POST'])
+def validate():
+    user_otp = request.form['otp']
+    if otp == int(user_otp) and recaptcha.verify():
+        return render_template('dashboard.html')
+    return "<h3>Error Recaptcha or Invalid OTP</h3>"
+
+
+
 @app.route('/signout')
 def signout():
-  return render_template('index.html')    
+    return render_template('index.html')
+
+
 
 if __name__ == '__main__':
     app.secret_key = 'mysecret'
